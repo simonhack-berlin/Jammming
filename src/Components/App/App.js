65077a92b6cc9b1a70c59c8e05b3d2b602d39b35
Simone Capuano <scapuano19@gmail.com>
2021-05-07 16:02:23 +0200

    Created Static Components

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import SearchBar from '../SearchBar/SearchBar';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+    <h1>Ja<span class="highlight">mmm</span>ing</h1>
+    <div class="App">
+      <SearchBar />
+      <div class="App-playlist">
+        {/* <!-- Add a SearchResults component -->
+        <!-- Add a Playlist component --> */}
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
