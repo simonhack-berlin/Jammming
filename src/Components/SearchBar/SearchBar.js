66621907e2d3a4824cd496cb81f60a797e526712
Simone Capuano <scapuano19@gmail.com>
2021-05-10 10:09:27 +0200

    Save a User's Playlist

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
+
 import './SearchBar.css';
 
-export default class SearchBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            term: ''
-        }
-        this.search = this.search.bind(this);
-        this.handleTermChange = this.handleTermChange.bind(this);
-    }
-    search() {
-        this.props.onSearch(this.state.term);
-    }
-    handleTermChange(event) {
-        this.setState({ term: event.target.value });
-    }
-    render() {
-        return (
-            <div className="SearchBar">
-  <input onChange={this.handleTermChange}
-  placeholder="Enter A Song, Album, or Artist" />
-  <button className="SearchButton">SEARCH</button>
-</div>
-        )
-    }
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  handleTermChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  search() {
+    this.props.onSearch(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <input placeholder="Enter A Song Title" onChange={this.handleTermChange} />
+        <button className="SearchButton" onClick={this.search}>SEARCH</button>
+      </div>
+    );
+  }
 }
+
+export default SearchBar;
